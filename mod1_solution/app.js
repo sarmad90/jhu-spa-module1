5eb9700ebe8cb59909e1b18ddcf9b874aaffc4ac
sarmad-sabih-10p <sam_sarmad@hotmail.com>
2016-09-16 12:22:36 +0500

    changes

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -1,7 +1,32 @@
-var lunchChecker = angular.module('lunchChecker', []);
-lunchChecker.controller('LunchCheckerController', LunchCheckerController {
-});
+(function(){
+  var LunchCheck = angular.module('LunchCheck', []);
+  LunchCheck.controller('LunchCheckerController', LunchCheckerController);
 
-function LunchCheckerController($scope){
+  LunchCheckerController.$inject = ['$scope'];
 
-};
+  function LunchCheckerController($scope){
+    $scope.dishes = "";
+    $scope.message = "";
+    $scope.color = "green";
+    $scope.border_style = "none";
+
+    $scope.checkLunch = function() {
+      $scope.border_style = "solid";
+      var arr = $scope.dishes.split(",");
+      arr = arr.filter(function(n){ return n.trim().length > 0 });
+      if ($scope.dishes.length < 1){
+        $scope.message = "Please enter data first";
+        $scope.color = "red";
+      }
+      else if (arr.length <= 3) {
+        $scope.message = "Enjoy!";
+        $scope.color = "green";
+      }
+      else{
+        $scope.message = "Too much!"; 
+        $scope.color = "green";
+      }
+    };
+
+  };  
+})();
